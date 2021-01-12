58110c681d4a3773164c00d17ac07e9b28d0c7b7
Herley Oliveira <herleyoliveira19@gmail.com>
2021-01-12 16:49:45 -0300

    adicionar o else na condição

--- src/fgts.py
+++ src/fgts.py
@@ -3,4 +3,7 @@ valor = float(input("Salário: "))
 fgts = (valor * 0.08)
 if (valor <= 1840):
     print (f"R${fgts :.2f} reais, este será o valor a ser recolhido.")
+else:
+    print('invaled, tente novamente')
+
 
