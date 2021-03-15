aabcf2efbdbfcb1b05da67c32f53d0d6c2ea9798
Herley Oliveira <herleyoliveira19@gmail.com>
2021-03-15 00:05:07 -0300

    chore: conditional function

---
+++ funcao.js
@@ -0,0 +1,9 @@
+function ParImpar(n) {
+    if (n % 2 == 0) {
+        return 'O número é par'
+    }else {
+        return 'O número é impar'
+    };
+};
+let res = ParImpar(4);
+console.log (res);
