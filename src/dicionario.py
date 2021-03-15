0c50f2b9e3a022de16cfedc4194ecfaabaaf3ee4
Herley Oliveira <herleyoliveira19@gmail.com>
2021-03-15 00:06:05 -0300

    chore: dictionary study

---
+++ src/dicionario.py
@@ -0,0 +1,26 @@
+# DICIONÁRIOS
+
+# - Não são propriedade sequencial, mas mais precisamente mapeamentos.
+# - Os objetos são armazenados por chaves em vez de posição relativa.
+# - São mutáveis.
+# - Codificados com cheve {}.
+# - Consistem em uma série de pares chave:valor.
+# - São úteis quando precisamos associar um conjunto de valores a chave 
+# - DICIONARIO = {"produto": "tigela", "cor": "vermelha", "preço": 14}
+# 
+# - Podemos indexar um dicionário para obter e alterar os valores associados às chaves.
+# - Porém usamos como índex uma chave, não uma posição relativa.
+# 
+# EX: dicionario["produto"] - Retorna o valor associado à chave produto.
+#  - dicionari["preço"] += 1 - aumenta em 1 o valor do preço.
+# 
+# -Criar dicionários 
+# 
+dicionario={}
+dicionario['nome']='Herley'
+dicionario["sobrenome"]="Oliveira"
+dicionario["empresa"]="Artisans"
+
+print(dicionario)
+ 
+    
